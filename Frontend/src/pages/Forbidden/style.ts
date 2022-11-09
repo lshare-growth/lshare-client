caea6acb5e1967dbb579aeed93c8dfd74c2e8291
khj <hongjung.kim.dev@gmail.com>
2022-11-10 02:41:41 +0900

    feat: 403페이지 불필요한 margin 제거
    - 403페이지 불필요한 margin 제거

--- Frontend/src/pages/Forbidden/style.ts
+++ Frontend/src/pages/Forbidden/style.ts
@@ -5,7 +5,6 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 160px;
   height: 100vh;
   vertical-align: 'middle';
   text-align: center;
